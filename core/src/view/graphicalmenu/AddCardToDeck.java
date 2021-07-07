4f259f5bb69528a426e4b0503770093990e915ac
Amirreza Ghadyani <77363478+amirrezaghadyani@users.noreply.github.com>
2021-07-07 18:40:52 +0430

    initial add card

--- core/src/view/graphicalmenu/AddCardToDeck.java
+++ core/src/view/graphicalmenu/AddCardToDeck.java
@@ -3,12 +3,18 @@ package view.graphicalmenu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.MyGdxGame;
+import model.Finisher;
+import model.card.Card;
 import model.user.User;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class AddCardToDeck implements Screen ,Input.TextInputListener {
     SpriteBatch batch;
@@ -17,12 +23,21 @@ public class AddCardToDeck implements Screen ,Input.TextInputListener {
     Texture wallpaper;
     BitmapFont text;
     BitmapFont text1;
+    BitmapFont type;
+    BitmapFont error;
     Texture mute;
     Texture unmute;
     boolean isMute;
     Texture backButton;
     User currentLoggedInUser;
     String cardNAme = "";
+    Texture cardName;
+    int message = 0;
+    Texture add;
+    String address = null;
+    boolean isNameCorrect = false;
+    Texture card;
+
 
     public AddCardToDeck(MyGdxGame game, boolean isMute, User currentLoggedInUser) {
         this.currentLoggedInUser = currentLoggedInUser;
@@ -32,11 +47,15 @@ public class AddCardToDeck implements Screen ,Input.TextInputListener {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1600, 960);
         text = new BitmapFont(Gdx.files.internal("Agency.fnt"));
+        type = new BitmapFont(Gdx.files.internal("Agency.fnt"));
+        error = new BitmapFont(Gdx.files.internal("Agency.fnt"));
         text1 = new BitmapFont(Gdx.files.internal("times.fnt"));
         wallpaper = new Texture("wallpaper.jpg");
         mute = new Texture("buttons/mute.png");
         unmute = new Texture("buttons/unmute.png");
         backButton = new Texture("buttons/back.png");
+        cardName = new Texture("buttons/cardName.png");
+        add = new Texture("buttons/addCard.png");
     }
 
     @Override
@@ -51,9 +70,32 @@ public class AddCardToDeck implements Screen ,Input.TextInputListener {
         batch.begin();
         batch.draw(wallpaper, 0, 0, 1600, 960);
         text.getData().setScale(0.3f);
+        type.getData().setScale(0.3f);
+        error.getData().setScale(0.2f);
+        type.setColor(Color.YELLOW);
         text1.draw(batch, "la nature est l'eglise de satan...", 1200, 30);
         text.draw(batch, "Add card to deck", 150, 850);
         batch.draw(backButton, 10, 10, backButton.getWidth(), backButton.getHeight());
+        batch.draw(cardName, 50, 300, cardName.getWidth(), cardName.getHeight());
+        batch.draw(add, 660, 180, add.getWidth(), add.getHeight());
+        type.draw(batch, cardNAme, 340, 380);
+        if (isNameCorrect && address != null) {
+            card = new Texture(address);
+            batch.draw(card, 1100, 150, card.getWidth(), card.getHeight());
+        }
+        if (message == 1) {
+            error.setColor(Color.RED);
+            error.draw(batch, "enter a name", 150, 280);
+        } else if (message == 2) {
+            error.setColor(Color.RED);
+            error.draw(batch, "incorrect card name!", 150, 280);
+        } else if (message == 3) {
+            error.setColor(Color.RED);
+            error.draw(batch, "not enough money!", 150, 280);
+        } else if (message == 4) {
+            error.setColor(Color.GREEN);
+            error.draw(batch, "card successfully bought", 150, 280);
+        }
         batch.end();
 
         if (Gdx.input.justTouched()) {
@@ -67,6 +109,38 @@ public class AddCardToDeck implements Screen ,Input.TextInputListener {
                 if (Gdx.input.getX() > 10 && Gdx.input.getX() < 10 + backButton.getWidth()) {
                     game.setScreen(new Decks(game, isMute, currentLoggedInUser));
                     dispose();
+                }
+            }
+
+            if (Gdx.input.getY() > 660 - cardName.getHeight() && Gdx.input.getY() < 660) {
+                if (Gdx.input.getX() > 50 && Gdx.input.getX() < 50 + cardName.getWidth()) {
+                    Gdx.input.getTextInput(this, "Card name", "", "");
+                }
+            }
+
+            if (Gdx.input.getY() > 780 - add.getHeight() && Gdx.input.getY() < 780) {
+                if (Gdx.input.getX() > 660 && Gdx.input.getX() < 660 + add.getWidth()) {
+                    message = 0;
+                    if (cardNAme.equals("")) {
+                        message = 1;
+                    } else if (getCardImageFileAddress(cardNAme) == null) {
+                        message = 2;
+                    } else {
+                        Card newCard = Objects.requireNonNull(Card.getCardByName(cardNAme));
+                        if (newCard.getPrice() > currentLoggedInUser.getCredit()) {
+                            message = 3;
+                        } else {
+                            message = 4;
+                            isNameCorrect = true;
+                            currentLoggedInUser.addCard(newCard);
+                            try {
+                                Finisher.finish();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            address = getCardImageFileAddress(cardNAme) + ".jpg";
+                        }
+                    }
                 }
             }
 
@@ -85,6 +159,163 @@ public class AddCardToDeck implements Screen ,Input.TextInputListener {
             batch.end();
         }
     }
+
+    public String getCardImageFileAddress(String input) {
+        if (input.equals("Battle OX"))
+            return "cards/monsters/BattleOx";
+        if (input.equals("Axe Raider"))
+            return "cards/monsters/AxeRaider";
+        if (input.equals("Yomi Ship"))
+            return "cards/monsters/YomiShip";
+        if (input.equals("Horn Imp"))
+            return "cards/monsters/HornImp";
+        if (input.equals("Silver Fang"))
+            return "cards/monsters/SilverFang";
+        if (input.equals("Suijin"))
+            return "cards/monsters/Suijin";
+        if (input.equals("Fireyarou"))
+            return "cards/monsters/Fireyarou";
+        if (input.equals("Curtain of the dark ones"))
+            return "cards/monsters/CurtainOfTheDarkOnes";
+        if (input.equals("Feral Imp"))
+            return "cards/monsters/FeralImp";
+        if (input.equals("Dark magician"))
+            return "cards/monsters/DarkMagician";
+        if (input.equals("Wattkid"))
+            return "cards/monsters/Wattkid";
+        if (input.equals("Baby dragon"))
+            return "cards/monsters/BabyDragon";
+        if (input.equals("Hero of the east"))
+            return "cards/monsters/HeroOfTheEast";
+        if (input.equals("Battle warrior"))
+            return "cards/monsters/BattleWarrior";
+        if (input.equals("Crawling dragon"))
+            return "cards/monsters/CrawlingDragon";
+        if (input.equals("Flame manipulator"))
+            return "cards/monsters/FlameManipulator";
+        if (input.equals("Blue-Eyes white dragon"))
+            return "cards/monsters/BlueEyesWhiteDragon";
+        if (input.equals("Crab Turtle"))
+            return "cards/monsters/CrabTurtle";
+        if (input.equals("Skull Guardian"))
+            return "cards/monsters/SkullGuardian";
+        if (input.equals("Slot Machine"))
+            return "cards/monsters/SlotMachine";
+        if (input.equals("Haniwa"))
+            return "cards/monsters/Haniwa";
+        if (input.equals("Man-Eater Bug"))
+            return "cards/monsters/ManEaterBug";
+        if (input.equals("Gate Guardian"))
+            return "cards/monsters/GateGuardian";
+        if (input.equals("Scanner"))
+            return "cards/monsters/Scanner";
+        if (input.equals("Bitron"))
+            return "cards/monsters/Bitron";
+        if (input.equals("Marshmallon"))
+            return "cards/monsters/Marshmallon";
+        if (input.equals("Beast King Barbaros"))
+            return "cards/monsters/BeastKingBarbaros";
+        if (input.equals("Texchanger"))
+            return "cards/monsters/Texchanger";
+        if (input.equals("Leotron ") || input.equals("Leotron"))
+            return "cards/monsters/Leotron";
+        if (input.equals("The Calculator"))
+            return "cards/monsters/TheCalculator";
+        if (input.equals("Alexandrite Dragon"))
+            return "cards/monsters/AlexandriteDragon";
+        if (input.equals("Mirage Dragon"))
+            return "cards/monsters/MirageDragon";
+        if (input.equals("Herald of Creation"))
+            return "cards/monsters/HeraldOfCreation";
+        if (input.equals("Exploder Dragon"))
+            return "cards/monsters/ExploderDragon";
+        if (input.equals("Warrior Dai Grepher"))
+            return "cards/monsters/WarriorDaiGrepher";
+        if (input.equals("Dark Blade"))
+            return "cards/monsters/DarkBlade";
+        if (input.equals("Wattaildragon"))
+            return "cards/monsters/Wattaildragon";
+        if (input.equals("Terratiger# the Empowered Warrior") || input.equals("Terratiger"))
+            return "cards/monsters/Terratiger";
+        if (input.equals("The Tricky"))
+            return "cards/monsters/TheTricky";
+        if (input.equals("Spiral Serpent"))
+            return "cards/monsters/SpiralSerpent";
+        if (input.equals("Command Knight"))
+            return "cards/monsters/CommandKnight";
+        if (input.equals("Trap Hole"))
+            return "cards/SpellTrap/TrapHole";
+        if (input.equals("Mirror Force"))
+            return "cards/SpellTrap/MirrorForce";
+        if (input.equals("Magic Cylinder"))
+            return "cards/SpellTrap/MagicCylinder";
+        if (input.equals("Mind Crush"))
+            return "cards/SpellTrap/MindCrush";
+        if (input.equals("Torrential Tribute"))
+            return "cards/SpellTrap/TorrentialTribute";
+        if (input.equals("Time Seal"))
+            return "cards/SpellTrap/TimeSeal";
+        if (input.equals("Negate Attack"))
+            return "cards/SpellTrap/NegateAttack";
+        if (input.equals("Solemn Warning"))
+            return "cards/SpellTrap/SolemnWarning";
+        if (input.equals("Magic Jamamer") || input.equals("Magic Jammer"))
+            return "cards/SpellTrap/Magic Jammer";
+        if (input.equals("Call of The Haunted"))
+            return "cards/SpellTrap/Call of the Hunted";
+        if (input.equals("Vanity's Emptiness"))
+            return "cards/SpellTrap/VanitysEmptiness";
+        if (input.equals("Wall of Revealing Light"))
+            return "cards/SpellTrap/WallOfRevealingLight";
+        if (input.equals("Monster Reborn"))
+            return "cards/SpellTrap/MonsterReborn";
+        if (input.equals("Terraforming"))
+            return "cards/SpellTrap/Terraforming";
+        if (input.equals("Pot of Greed"))
+            return "cards/SpellTrap/PotOfGreed";
+        if (input.equals("Raigeki"))
+            return "cards/SpellTrap/Raigeki";
+        if (input.equals("Change of Heart"))
+            return "cards/SpellTrap/ChangeOfHeart";
+        if (input.equals("Swords of Revealing Light"))
+            return "cards/SpellTrap/SwordOfRevealingLight";
+        if (input.equals("Harpie's Feather Duster"))
+            return "cards/SpellTrap/HarpiesFeatherDuster";
+        if (input.equals("Dark Hole"))
+            return "cards/SpellTrap/DarkHole";
+        if (input.equals("Supply Squad"))
+            return "cards/SpellTrap/SupplySquad";
+        if (input.equals("Spell Absorption"))
+            return "cards/SpellTrap/SpellAbsorption";
+        if (input.equals("Messenger of peace"))
+            return "cards/SpellTrap/MessengerOfPeace";
+        if (input.equals("Twin Twisters"))
+            return "cards/SpellTrap/TwinTwisters";
+        if (input.equals("Mystical space typhoon"))
+            return "cards/SpellTrap/MysticalSpaceTyphoon";
+        if (input.equals("Ring of defense"))
+            return "cards/SpellTrap/RingOfDefense";
+        if (input.equals("Yami"))
+            return "cards/SpellTrap/Yami";
+        if (input.equals("Forest"))
+            return "cards/SpellTrap/Forest";
+        if (input.equals("Closed Forest"))
+            return "cards/SpellTrap/ClosedForest";
+        if (input.equals("Umiiruka"))
+            return "cards/SpellTrap/Umiiruka";
+        if (input.equals("Sword of dark destruction"))
+            return "cards/SpellTrap/SwordOfDarkDestruction";
+        if (input.equals("Black Pendant"))
+            return "cards/SpellTrap/BlackPendant";
+        if (input.equals("United We Stand"))
+            return "cards/SpellTrap/UnitedWeStand";
+        if (input.equals("Magnum Shield"))
+            return "cards/SpellTrap/MagnumShield";
+        if (input.equals("Advanced Ritual Art"))
+            return "cards/SpellTrap/AdvancedRitualArt";
+        return null;
+    }
+
 
     @Override
     public void resize(int width, int height) {
