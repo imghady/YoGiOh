3f14fbfdab39cd9c75a53f7b8bfdf693e588826e
Amirreza Ghadyani <77363478+amirrezaghadyani@users.noreply.github.com>
2021-07-10 06:08:06 +0430

    edit button

--- core/src/view/graphicalmenu/SelectType.java
+++ core/src/view/graphicalmenu/SelectType.java
@@ -1,4 +1,128 @@
 package view.graphicalmenu;
 
-public class SelectType {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Mola;
+import model.user.User;
+
+public class SelectType implements Screen {
+
+    SpriteBatch batch;
+    final Mola game;
+    OrthographicCamera camera;
+    Texture wallpaper;
+    Texture types;
+    BitmapFont text;
+    BitmapFont text1;
+    Texture mute;
+    Texture unmute;
+    boolean isMute = false;
+    Texture backButton;
+    User currentLoggedInUser;
+
+    public SelectType(Mola game, boolean isMute, User currentLoggedInUser) {
+        this.currentLoggedInUser = currentLoggedInUser;
+        this.isMute = isMute;
+        this.game = game;
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1600, 960);
+        text = new BitmapFont(Gdx.files.internal("Agency.fnt"));
+        text1 = new BitmapFont(Gdx.files.internal("times.fnt"));
+        wallpaper = new Texture("wallpaper.jpg");
+        mute = new Texture("buttons/mute.png");
+        unmute = new Texture("buttons/unmute.png");
+        backButton = new Texture("buttons/back.png");
+        types = new Texture("buttons/types.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(wallpaper, 0, 0, 1600, 960);
+        text.getData().setScale(0.2f);
+        text1.draw(batch, "la nature est l'eglise de satan...", 1200, 30);
+        text.draw(batch, "select type for create card", 150, 850);
+        batch.draw(backButton, 10, 10, backButton.getWidth(), backButton.getHeight());
+        batch.draw(types, 150, 200, types.getWidth(), types.getHeight());
+        batch.end();
+
+        if (Gdx.input.justTouched()) {
+
+            if (Gdx.input.getX() > 10 && Gdx.input.getX() < 10 + mute.getWidth()
+                    && Gdx.input.getY() < 110 && Gdx.input.getY() > 110 - mute.getHeight()) {
+                isMute = !isMute;
+            }
+
+            if (Gdx.input.getY() > 950 - backButton.getHeight() && Gdx.input.getY() < 950) {
+                if (Gdx.input.getX() > 10 && Gdx.input.getX() < 10 + backButton.getWidth()) {
+                    game.setScreen(new ImportExport(game, isMute, currentLoggedInUser));
+                    dispose();
+                }
+            }
+
+            if (Gdx.input.getX() > 150 && Gdx.input.getX() < 150 + types.getWidth()) {
+                if (Gdx.input.getY() > 760 - types.getHeight() / 3 && Gdx.input.getY() < 760) {
+                    game.setScreen(new ShowAllCards1(game, isMute, currentLoggedInUser, "shop"));
+                    dispose();
+                } else if (Gdx.input.getY() > 760 - 2 * types.getHeight() / 3 && Gdx.input.getY() < 760 - types.getHeight() / 3) {
+                    game.setScreen(new ShowOneCard(game, isMute, currentLoggedInUser));
+                    dispose();
+                } else if (Gdx.input.getY() > 760 - types.getHeight() && Gdx.input.getY() < 760 - 2 * types.getHeight() / 3) {
+                    game.setScreen(new BuyCard(game, isMute, currentLoggedInUser));
+                    dispose();
+                }
+            }
+
+        }
+
+
+        if (isMute) {
+            batch.begin();
+            batch.draw(mute, 10, 850, mute.getWidth(), mute.getHeight());
+            Mola.music.pause();
+            batch.end();
+        } else {
+            batch.begin();
+            batch.draw(unmute, 10, 850, unmute.getWidth(), unmute.getHeight());
+            Mola.music.play();
+            batch.end();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
