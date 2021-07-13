f6720f7e781741f11b38459957cdb2f9779b8786
AliMarshmallow <79265188+AliMarshmallow@users.noreply.github.com>
2021-07-13 19:34:34 +0430

    buy

--- core/src/controller/ShopMenu.java
+++ core/src/controller/ShopMenu.java
@@ -1,17 +1,22 @@
 package controller;
 
+import com.google.gson.Gson;
 import model.card.Card;
 import model.card.Monster;
 import model.card.Spell;
 import model.card.Trap;
 import model.user.User;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import view.TerminalOutput;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ShopMenu {
 
     private User currentUser;
+    private JSONParser parser = new JSONParser();
 
     public ShopMenu(String username) {
         this.currentUser = User.getUserByUsername(username);
@@ -21,37 +26,40 @@ public class ShopMenu {
 
     }
 
-    public void buyCard(String cardName) {
-        if (Card.getCardByName(cardName) == null) {
-            TerminalOutput.output("There is no card with this name");
-            return;
+    public String buyCard(String cardName) {
+        JSONObject jsonObject =new JSONObject();
+        jsonObject.put("command","shop");
+        jsonObject.put("cardName",cardName);
+        jsonObject.put("token",AppClient.getToken());
+        try {
+            AppClient.dataOutputStream.writeUTF(jsonObject.toJSONString());
+            AppClient.dataOutputStream.flush();
+            String result = AppClient.dataInputStream.readUTF();
+            String[] data = result.split("#####");
+            jsonObject = (JSONObject) parser.parse(data[0]);
+            if (jsonObject.get("type").equals("Error")){
+                return jsonObject.get("message").toString();
+            }
+            else if (jsonObject.get("type").equals("monster")){
+                System.out.println("kir1");
+                Gson gson = new Gson();
+                currentUser.addCard(gson.fromJson(data[1],Monster.class));
+                return "success";
+            }else if (jsonObject.get("type").equals("trap")){
+                System.out.println("kir2");
+                Gson gson = new Gson();
+                currentUser.addCard(gson.fromJson(data[1],Trap.class));
+                return "success";
+            }else if (jsonObject.get("type").equals("spell")){
+                System.out.println("kir3");
+                Gson gson = new Gson();
+                currentUser.addCard(gson.fromJson(data[1],Spell.class));
+                return "success";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Card card = Card.getCardByName(cardName);
-        if (card.getPrice() > currentUser.getCredit()) {
-            TerminalOutput.output("not enough money");
-            return;
-        }
-        if (card instanceof Monster) {
-            Monster monster = (Monster) card;
-            Monster monster1 = new Monster(monster.getName(), monster.getLevel(), monster.getAttribute()
-                    , monster.getMonsterType(), monster.getCardType(), monster.getAttack(), monster.getDefence(), monster.getDescription(), monster.getPrice());
-            currentUser.addCard(monster1);
-            TerminalOutput.output("Card Buy Successful.");
-            return;
-        }
-        if (card instanceof Spell) {
-            Spell spell = (Spell) card;
-            Spell spell1 = new Spell(spell.getName(), spell.getIcon(), spell.getDescription(), spell.getStatus(), spell.getPrice());
-            currentUser.addCard(spell1);
-            TerminalOutput.output("Card Buy Successful.");
-            return;
-        }
-        if (card instanceof Trap) {
-            Trap trap = (Trap) card;
-            Trap trap1 = new Trap(trap.getName(), trap.getIcon(), trap.getDescription(), trap.getStatus(), trap.getPrice());
-            currentUser.addCard(trap1);
-            TerminalOutput.output("Card Buy Successful.");
-        }
+        return "";
     }
 
     public void showAllCard() {
